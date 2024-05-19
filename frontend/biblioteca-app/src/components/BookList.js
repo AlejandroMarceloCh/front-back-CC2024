6e2d3350fe881135adf36d582d6d3c75f7ac2712
Alejandro Marcelo <alejandromarcelo@MacBook-Pro-de-Alejandro.local>
2024-05-19 07:49:45 -0500

    BookList.js

--- frontend/biblioteca-app/src/components/BookList.js
+++ frontend/biblioteca-app/src/components/BookList.js
@@ -1,32 +1,47 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import '../styles/BookList.css';
 
 const BookList = () => {
     const [books, setBooks] = useState([]);
+    const [showBooks, setShowBooks] = useState(false);
 
-    useEffect(() => {
-        const fetchBooks = async () => {
-            try {
-                const response = await axios.get('http://34.203.174.121:8001/libros');
-                setBooks(response.data.libros);
-            } catch (error) {
-                alert('Error fetching books');
-            }
-        };
-
-        fetchBooks();
-    }, []);
+    const fetchBooks = async () => {
+        try {
+            const response = await axios.get('http://34.203.174.121:8001/libros');
+            setBooks(response.data.libros);
+            setShowBooks(true);
+        } catch (error) {
+            alert('Error fetching books');
+        }
+    };
 
     return (
-        <ul className="book-list">
-            {books.map((book) => (
-                <li key={book.id}>
-                    <span className="book-title">{book.titulo}</span>
-                    <span className="book-author">by {book.autor}</span>
-                </li>
-            ))}
-        </ul>
+        <div className="book-list-container">
+            <button className="fetch-books-button" onClick={fetchBooks}>
+                Mostrar Libros
+            </button>
+            {showBooks && (
+                <table className="book-table">
+                    <thead>
+                        <tr>
+                            <th>Título</th>
+                            <th>Autor</th>
+                            <th>Año</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {books.map((book) => (
+                            <tr key={book.id}>
+                                <td>{book.titulo}</td>
+                                <td>{book.autor}</td>
+                                <td>{book.ano}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
+        </div>
     );
 };
 
